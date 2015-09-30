aae298eb52319569caf152ebd6ba2f6256a13155
Virendra <vpa.narola@narolainfotech.com>
2015-09-30 12:16:14 +0530

    Working on Notice Copytoclipboard functuionality

--- Code/application/views/Templates/Admin/default.php
+++ Code/application/views/Templates/Admin/default.php
@@ -28,7 +28,8 @@
     <script src="assets/js/jquery-ui.min.js"></script> <!-- jQuery UI JS -->
     <script src="assets/js/bootstrap.min.js"></script> <!-- Bootstap JS -->
     <script src="assets/js/select2.min.js"></script> <!-- Select2 JS -->
-    
+    <script src="assets/js/ZeroClipboard.min.js"></script>  <!-- For Copy to Clipboard Functionality Refer:http://zeroclipboard.org/ -->
+
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
