c39d3c2c652fb70d5f3d716169b24e41514c58d3
Virendra <vpa.narola@narolainfotech.com>
2015-10-07 12:21:41 +0530

    Bug Fixing

--- Code/application/libraries/ADMIN_Controller.php
+++ Code/application/libraries/ADMIN_Controller.php
@@ -7,33 +7,22 @@ class ADMIN_Controller extends CI_Controller {
 		
 		parent::__construct();
 		
-		$exceptional_url = array('admin','admin/logout');
-		
-		
-			$cur_url = $this->session->userdata('cur_url'); echo "<br/>";
-		 	$prev_url = $this->session->userdata('prev_url'); echo "<br/>";
-		 	echo $test_url = $cur_url;	
-
-		$test_url = $cur_url;
-
+		$cur_url = $this->session->userdata('cur_url'); 
+	 	$prev_url = $this->session->userdata('prev_url'); 
+	 		
 		if(empty($cur_url) && empty($prev_url)){
 			
 			$this->session->set_userdata( array('cur_url'=>$this->input->server('REQUEST_URI')) );
 			$this->session->set_userdata( array('prev_url'=>'test') );
 		}
 
-		p($_SESSION);
-		
-		if(!empty($cur_url)){
-
-			if( $this->input->server('REQUEST_URI') != $cur_url ){
-				
-				$this->session->set_userdata( array('cur_url'=>$this->input->server('REQUEST_URI')) );
-				$this->session->set_userdata( array('prev_url'=>$cur_url) );	
-
-			}
-				
+		if(!empty($cur_url) && $this->input->server('REQUEST_URI') != $cur_url ){
+			
+			$this->session->set_userdata( array('cur_url'=>$this->input->server('REQUEST_URI')) );
+			$this->session->set_userdata( array('prev_url'=>$cur_url) );	
 		}
+
+		$exceptional_url = array('admin','admin/logout');
 
 		if(in_array(uri_string(), $exceptional_url) == FALSE && is_loggedin_admin() == FALSE){
 
