c412ae4be824bd79ebe4eda5e226d2bbdbd23245
Hiren Danecha <hid.narola@narolainfotech.com>
2015-10-15 18:00:05 +0530

    School , Subject module changes

--- Code/application/views/Templates/Admin/default.php
+++ Code/application/views/Templates/Admin/default.php
@@ -96,7 +96,9 @@ $url = uri_string();
                         <?php
                         if (in_array($url, array('admin/user', 'admin/topic/allocate',
                                     'admin/topic/lists', 'admin/school', 'admin/school/add',
-                                    'admin/classroom', 'admin/classroom/add'))) {
+                                    'admin/classroom', 'admin/classroom/add',
+                                    'admin/subject/lists', 'admin/subject/add_subject',
+                                ))) {
                             echo 'active';
                         } else {
 
@@ -104,7 +106,6 @@ $url = uri_string();
                                 echo 'active';
                             }
                         }
-
                         ?> dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                                 <span class="icon icon_menu_manage"></span> Manage
@@ -113,7 +114,7 @@ $url = uri_string();
                                 <li><a href="#">Book</a></li>
                                 <li><a href="#">Auther</a></li>
                                 <li><a href="admin/school">School</a></li>                                
-                               <li class="dropdown sub_menu">
+                                <li class="dropdown sub_menu">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Subjects</a>
                                     <ul class="dropdown-menu">
                                         <li><a href="admin/subject/lists">List of Subjects</a></li>
@@ -217,9 +218,9 @@ $url = uri_string();
                             <a href="#"><span class="icon icon_banner"></span>Banners</a>
                         </li>                   
                         <li class="<?php
-                        if (in_array($url, array('admin/notice', 'admin/notice/index'))) {
-                            echo 'active';
-                        }
+                            if (in_array($url, array('admin/notice', 'admin/notice/index'))) {
+                                echo 'active';
+                            }
                         ?>">
                             <a href="admin/notice"><span class="icon icon_notice"></span>Notice Board</a>
                         </li>
@@ -433,32 +434,34 @@ $url = uri_string();
 
 
         <script src="assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
-        
+
 
         <script type="text/javascript">
 
             $(function () {
-              $('[data-toggle="popover"]').popover();
+                $('[data-toggle="popover"]').popover();
             });
 
-            jQuery(document).ready(function(){
-            jQuery('.exam_year .icon_option_dark').click(function(){
-                if(jQuery(this).parent().children('.popover').css('display')=='block'){
-                    jQuery(this).parent().children('.popover').css('display','none');
-                }
-                else{
-                    jQuery(this).parent().children('.popover').css('display','block');
-                };
+            jQuery(document).ready(function () {
+                jQuery('.exam_year .icon_option_dark').click(function () {
+                    if (jQuery(this).parent().children('.popover').css('display') == 'block') {
+                        jQuery(this).parent().children('.popover').css('display', 'none');
+                    }
+                    else {
+                        jQuery(this).parent().children('.popover').css('display', 'block');
+                    }
+                    ;
+                });
+                jQuery('.switch_btns .btn').click(function () {
+                    if (jQuery(this).hasClass('no_btn')) {
+                        jQuery('.switch_btns .btn').removeClass('btn_red');
+                        jQuery('.switch_btns .btn').addClass('no_btn');
+                        jQuery(this).addClass('btn_red');
+                        jQuery(this).removeClass('no_btn');
+                    }
+                    ;
+                });
             });
-            jQuery('.switch_btns .btn').click(function(){
-                if(jQuery(this).hasClass('no_btn')){
-                    jQuery('.switch_btns .btn').removeClass('btn_red');                 
-                    jQuery('.switch_btns .btn').addClass('no_btn');
-                    jQuery(this).addClass('btn_red');
-                    jQuery(this).removeClass('no_btn');                 
-                };
-            });
-        });
 
             $('#birthdate input').datepicker({
                 format: 'yyyy-mm-dd'
--- Code/application/views/admin/subject/add_subject.php
+++ Code/application/views/admin/subject/add_subject.php
@@ -6,7 +6,7 @@
         <div class="col-sm-12">
             <ol class="breadcrumb">
                 <li><a href="admin/user">Admin</a></li>                          
-                <li><a href="admin/course/lists">Subjects</a></li>
+                <li><a href="admin/subject/lists">Subjects</a></li>
                 <li class="active">Add Subject</li>
             </ol>
         </div>
@@ -17,30 +17,71 @@
     <!--//filter-->
 
     <!--message-->
-   	<div class="row">
+    <div class="row">
         <div class="col-sm-12 new_message">
             <div class="box exam_card">
                 <div class="box_header">
                     <h3>Add Subject</h3>
                 </div>
+                <?php //p($courses); exit;?>
                 <form method="post" enctype='multipart/form-data'>
                     <div class="box_body ">
+                        <div class="form-group ">
+                            <label>Select Course</label>
+                            <select class="form-control " name="course" onchange="get_classroom(this.value)" id="course_id">
+                                <option selected disabled>Select Course</option> 
+                                <?php
+                                if (!empty($courses)) {
+                                    foreach ($courses as $course) {
+                                        ?> 
+                                        <option value="<?php echo $course['id']; ?>" <?php echo set_select('course', $course['id']); ?>> 
+                                            <?php echo $course['course_name']; ?></option>
+                                        <?php
+                                    }
+                                } else {
+                                    ?>
+                                    <option value="0"> No Course</option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <?php echo myform_error('course'); ?>
+
+                        <div class="form-group ">
+                            <label>Select Classroom</label>
+                            <select class="form-control " name="classroom" id="classroom_id">
+                                <option selected disabled>Select Classroom</option> 
+                                <?php
+                                if (!empty($classrooms)) {
+                                    foreach ($classrooms as $classroom) {
+                                        ?> 
+                                        <option value="<?php echo $classroom['id']; ?>" <?php echo set_select('classroom', $classroom['id']); ?>> 
+                                            <?php echo $classroom['class_name']; ?></option>
+                                        <?php
+                                    }
+                                } else {
+                                    ?>
+                                    <option value="0"> No Classroom</option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <?php echo myform_error('classroom'); ?>
+
                         <div class="form-group">
                             <label>Subject Image</label>
-                            <input type="file" class="form-control" name="subject_image">
+                            <input type="file"  name="subject_image">
                         </div>
-                    </div>
-                    <div class="box_body ">
+
                         <div class="form-group">
                             <label>Subject Name</label>
-                            <input type="text" class="form-control" name="subject_name" value="<?php echo set_value("subject_name"); ?>">
+                            <input type="text" class="form-control" name="subject_name" maxlength="30"
+                                   value="<?php echo set_value("subject_name"); ?>">
                         </div>                        
                         <?php echo myform_error('subject_name'); ?>
-                    </div>
-                    <div class="box_footer">
-                         <button type="submit" class="btn btn_green">Save</button>
-                        <button type="reset" class="btn btn_red">Reset</button>
-                        <a href="<?php echo base_url().$prev_url; ?>" class='btn btn_black'>Cancel</a>
+                        <div class="box_footer">
+                            <button type="submit" class="btn btn_green">Save</button>
+                            <button type="reset" class="btn btn_red">Reset</button>
+                            <a href="<?php echo base_url() . $prev_url; ?>" class='btn btn_black'>Cancel</a>
+                        </div>
                     </div>
                 </form>   
             </div>
@@ -49,3 +90,16 @@
     <!--//mesage-->
 </div>
 <!--//main-->        
+
+<script>
+    function get_classroom(course_id) {
+        $.ajax({
+            url: '<?php echo base_url() . "common/ajax_get_classrooms"; ?>',
+            type: 'POST',
+            data: {course_id: course_id},
+            success: function (data) {
+                $('#classroom_id').html(data);
+            }
+        });
+    }
+</script>
