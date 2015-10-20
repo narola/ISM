bf37dc2f1c466df7c37f268b2b712b217fad5db5
MubinaShaikh <c166@C166s-Mac-Pro.local>
2015-10-20 16:01:12 +0530

    added the login and signup screen

--- ISM/app/src/teacher_module/java/com/ism/teacher/helper/ReadJson.java
+++ ISM/app/src/teacher_module/java/com/ism/teacher/helper/ReadJson.java
@@ -1,41 +1,41 @@
-package com.ism.teacher.helper;
-
-import android.content.Context;
-
-import com.zota.davaindia.R;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-public class ReadJson {
-
-	private Context _context;
-
-	public ReadJson(Context context){
-		this._context = context;
-	}
-
-    public String ReadEventInfo()
-    {
-        InputStream inputStream = _context.getResources().openRawResource(R.raw.store_detail_response);
-        System.out.println(inputStream);
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        int i;
-        try {
-            i = inputStream.read();
-            while (i != -1)
-            {
-                byteArrayOutputStream.write(i);
-                i = inputStream.read();
-            }
-            inputStream.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return byteArrayOutputStream.toString();
-    }
-}
+//package com.ism.teacher.helper;
+//
+//import android.content.Context;
+//
+//import com.ism.R;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//
+//public class ReadJson {
+//
+//	private Context _context;
+//
+//	public ReadJson(Context context){
+//		this._context = context;
+//	}
+//
+//    public String ReadEventInfo()
+//    {
+//        InputStream inputStream = _context.getResources().openRawResource(R.raw.store_detail_response);
+//        System.out.println(inputStream);
+//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//
+//        int i;
+//        try {
+//            i = inputStream.read();
+//            while (i != -1)
+//            {
+//                byteArrayOutputStream.write(i);
+//                i = inputStream.read();
+//            }
+//            inputStream.close();
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//        return byteArrayOutputStream.toString();
+//    }
+//}
