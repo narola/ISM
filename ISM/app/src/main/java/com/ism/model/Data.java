ea366ca7bae6246d4ef376154e7dc1a31cea66f5
Krunal Panchal <c161@C161s-Mac-Pro.local>
2015-10-23 14:27:21 +0530

    Webservice helper classes

---
+++ ISM/app/src/main/java/com/ism/model/Data.java
@@ -0,0 +1,78 @@
+package com.ism.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.json.*;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Data {
+	
+    private String schoolName;
+    private String courseId;
+    private String courseName;
+    private String roleId;
+    private String schoolId;
+    private String academicYear;
+
+
+    @JsonProperty("school_name")
+    public String getSchoolName() {
+        return this.schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    @JsonProperty("course_id")
+	public String getCourseId() {
+        return this.courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    @JsonProperty("course_name")
+    public String getCourseName() {
+        return this.courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    @JsonProperty("role_id")
+    public String getRoleId() {
+        return this.roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    @JsonProperty("school_id")
+    public String getSchoolId() {
+        return this.schoolId;
+    }
+
+    public void setSchoolId(String schoolId) {
+        this.schoolId = schoolId;
+    }
+
+    @JsonProperty("academic_year")
+    public String getAcademicYear() {
+        return this.academicYear;
+    }
+
+    public void setAcademicYear(String academicYear) {
+        this.academicYear = academicYear;
+    }
+
+
+    
+}
--- ISM/app/src/main/java/com/ism/ws/RequestWs.java
+++ ISM/app/src/main/java/com/ism/ws/RequestWs.java
@@ -2,25 +2,25 @@ package com.ism.ws;
 
 import java.util.List;
 
-import org.apache.http.NameValuePair;
+//import org.apache.http.NameValuePair;
+
 
 public class RequestWs {
 
-	/** Get Request **/
-	public <CLS> CLS getGetRequest(String url, Class<CLS> cls) throws Exception {
-		return new WebServiceRequestGet(url).execute(cls);
-	}
+//	/** Get Request **/
+//	public <CLS> CLS getGetRequest(String url, Class<CLS> cls) throws Exception {
+//		return new WebServiceRequest(url).execute(cls);
+//	}
 
 	/** Post Request **/
-	public <CLS> CLS getPostRequest(String url, Class<CLS> cls, Object reqCls,
-			List<NameValuePair> nameValuePair) throws Exception {
-		if (nameValuePair != null) {
-			return new WebServiceRequestPost(url).execute(nameValuePair, cls,
+	public <CLS> CLS getRequest(String url, Class<CLS> cls, Object reqCls) throws Exception {
+		/*if (nameValuePair != null) {
+			return new WebServiceRequest(url).execute(nameValuePair, cls,
 					null);
-		} else {
-			return new WebServiceRequestPost(url).execute(null, cls, reqCls);
+		} else {*/
+			return new WSRequestPost(url).execute(cls, reqCls);
 
-		}
+//		}
 	}
 
 	/** Post Request upload file **/
