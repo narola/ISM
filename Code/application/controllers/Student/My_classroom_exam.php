4db514abd390076f113536a820e6a266daa064a4
Sandip Gopani <sag.narola@narolainfotech.com>
2015-10-28 09:34:15 +0530

    check for updates just test.

--- Code/application/controllers/Student/My_classroom_exam.php
+++ Code/application/controllers/Student/My_classroom_exam.php
@@ -36,7 +36,6 @@ class My_classroom_exam extends ISM_Controller {
 			$data['exam_type'] = '';
 
 		}
-		
 		//	get classroom subject		
 		$option = array('join' =>
 				array(
@@ -59,7 +58,8 @@ class My_classroom_exam extends ISM_Controller {
 				)
 		);
 
-		$data['my_exam']  = select(TBL_EXAMS.' e','if(TIMESTAMPDIFF(SECOND,NOW(),sc.created_date + Interval e.duration minute) < 0,0,TIMESTAMPDIFF(SECOND,NOW(),sc.created_date + Interval e.duration minute)) as remaining_time,sc.exam_status,e.exam_name,e.id as exam_id,TRUNCATE((sc.correct_answers * 100 / (select count(*) from '.TBL_EXAM_QUESTION.' where exam_id = sc.exam_id)),2) as per,e.subject_id,sc.id',$where1,$option);
+		$data['my_exam']  = select(TBL_EXAMS.' e','sc.exam_status,e.exam_name,e.id as exam_id,TRUNCATE((sc.correct_answers * 100 / (select count(*) from '.TBL_EXAM_QUESTION.' where exam_id = sc.exam_id)),2) as per,e.subject_id,sc.id',$where1,$option);
+		p($data,true);
 		$this->template->load('student/default','student/my_classroom_exam',$data);
 	}
 
