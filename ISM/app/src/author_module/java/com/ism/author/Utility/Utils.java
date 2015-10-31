865ea4db98a6b84686922376111e40fc9be92dd3
Arti Patel <c162@C162s-Mac-Pro.local>
2015-10-31 09:21:40 -0700

    add Post utility on author module

--- ISM/app/src/author_module/java/com/ism/author/Utility/Utils.java
+++ ISM/app/src/author_module/java/com/ism/author/Utility/Utils.java
@@ -9,13 +9,10 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
-<<<<<<< HEAD
-import java.text.SimpleDateFormat;
-=======
 import com.ism.author.fragment.TrialActivityFragment;
 import com.ism.utility.Utility;
 
->>>>>>> 13f8af244f1c82ba5ddc2ba13a29330a4d7b6164
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 /**
@@ -70,12 +67,12 @@ public class Utils {
 
     }
 
-<<<<<<< HEAD
     public static String getDate() {
         SimpleDateFormat curFormater = new SimpleDateFormat("yyyy-MM-dd");
         Calendar calendar = Calendar.getInstance();
         return curFormater.format(calendar.getTime());
-=======
+    }
+
     private static DatePickerDialog datePickerDob;
     private static Calendar calDob;
     private static String strDob;
@@ -105,7 +102,6 @@ public class Utils {
         }
 
         return strDob;
->>>>>>> 13f8af244f1c82ba5ddc2ba13a29330a4d7b6164
     }
 
 
