cac48869b2bf54cf4f101e0235787557dbdef95d
Krunal Panchal <c161@C161s-Mac-Pro.local>
2015-11-23 18:26:13 +0530

    Connection alert update

--- ISM/app/src/main/java/com/ism/ws/helper/WebserviceWrapper.java
+++ ISM/app/src/main/java/com/ism/ws/helper/WebserviceWrapper.java
@@ -160,7 +160,7 @@ public class WebserviceWrapper {
 
         @Override
         protected void onPostExecute(Object responseObject) {
-            if (isNetworkConnected) {
+            if (!isNetworkConnected) {
                 Utility.alert(context.getApplicationContext(), context.getString(R.string.connectivity_problem), context.getString(R.string.msg_server_connection));
             }
 	        webserviceResponse.onResponse(responseObject, exception, currentApiCode);
