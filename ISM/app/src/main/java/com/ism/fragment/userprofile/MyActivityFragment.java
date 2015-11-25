4bb5c93ecc6d54a1c01b374dae9f88b04616b641
Krunal Panchal <c161@C161s-Mac-Pro.local>
2015-11-25 18:18:50 +0530

    MyFeed complete

--- ISM/app/src/main/java/com/ism/fragment/userprofile/MyActivityFragment.java
+++ ISM/app/src/main/java/com/ism/fragment/userprofile/MyActivityFragment.java
@@ -1,16 +1,22 @@
-package com.ism.fragment;
+package com.ism.fragment.userprofile;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ism.R;
 import com.ism.activity.HostActivity;
+import com.ism.adapter.MyActivityAdapter;
 import com.ism.interfaces.FragmentListener;
 import com.ism.utility.Debug;
+
+import java.util.ArrayList;
 
 /**
  * Created by c161 on 06/11/15.
@@ -20,9 +26,11 @@ public class MyActivityFragment extends Fragment {
 	private static final String TAG = MyActivityFragment.class.getSimpleName();
 
 	private View view;
+	private RecyclerView recyclerMyActivity;
 
 	private HostActivity activityHost;
 	private FragmentListener fragListener;
+	private MyActivityAdapter adpMyActivity;
 
 	public static MyActivityFragment newInstance() {
 		MyActivityFragment fragment = new MyActivityFragment();
@@ -33,8 +41,7 @@ public class MyActivityFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	                         Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.fragment_my_activity, container, false);
 
 		initGlobal();
@@ -43,6 +50,29 @@ public class MyActivityFragment extends Fragment {
 	}
 
 	private void initGlobal() {
+		recyclerMyActivity = (RecyclerView) view.findViewById(R.id.recycler_my_activity);
+
+		recyclerMyActivity.setLayoutManager(new LinearLayoutManager(activityHost));
+		RecyclerView.ItemDecoration itemDecoration = new RecyclerView.ItemDecoration() {
+			@Override
+			public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+				super.getItemOffsets(outRect, view, parent, state);
+				outRect.bottom = 10;
+				outRect.left = 10;
+				outRect.right = 10;
+				if (parent.getChildLayoutPosition(view) == 0) {
+					outRect.top = 10;
+				}
+			}
+		};
+		recyclerMyActivity.addItemDecoration(itemDecoration);
+
+		ArrayList<String> arrayList = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			arrayList.add("List Item : " + i);
+		}
+		adpMyActivity = new MyActivityAdapter(activityHost, arrayList);
+		recyclerMyActivity.setAdapter(adpMyActivity);
 
 	}
 
