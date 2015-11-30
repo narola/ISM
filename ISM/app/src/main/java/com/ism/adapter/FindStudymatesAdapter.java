374bdea245c86fbb7b7e13b38febce69e22ec3ef
Krunal Panchal <c161@C161s-Mac-Pro.local>
2015-11-30 18:22:46 +0530

    Tutorial fragment began

--- ISM/app/src/main/java/com/ism/adapter/FindStudymatesAdapter.java
+++ ISM/app/src/main/java/com/ism/adapter/FindStudymatesAdapter.java
@@ -16,17 +16,17 @@ import com.ism.ws.model.User;
 import java.util.ArrayList;
 
 /**
- * Created by c161 on 28/11/15.
+ * Created by c161 on 30/11/15.
  */
-public class YourStudymatesAdapter extends RecyclerView.Adapter<YourStudymatesAdapter.ViewHolder> {
+public class FindStudymatesAdapter extends RecyclerView.Adapter<FindStudymatesAdapter.ViewHolder> {
 
-	private static final String TAG = YourStudymatesAdapter.class.getSimpleName();
+	private static final String TAG = FindStudymatesAdapter.class.getSimpleName();
 
 	private Context context;
 	private MyTypeFace myTypeFace;
 	private ArrayList<User> arrayListUser;
 
-	public YourStudymatesAdapter(Context context, ArrayList<User> arrListUser) {
+	public FindStudymatesAdapter(Context context, ArrayList<User> arrListUser) {
 		this.context = context;
 		this.arrayListUser = arrListUser;
 		myTypeFace = new MyTypeFace(context);
@@ -37,7 +37,7 @@ public class YourStudymatesAdapter extends RecyclerView.Adapter<YourStudymatesAd
 		View viewStatus;
 		CircleImageView imgDp;
 		TextView txtUserName, txtSchool, txtLocation, txtFollowing;
-		Button btnViewProfile;
+		Button btnAddStudymate;
 
 		public ViewHolder(View view) {
 			super(view);
@@ -47,24 +47,32 @@ public class YourStudymatesAdapter extends RecyclerView.Adapter<YourStudymatesAd
 			txtSchool = (TextView) view.findViewById(R.id.txt_school);
 			txtLocation = (TextView) view.findViewById(R.id.txt_location);
 			txtFollowing = (TextView) view.findViewById(R.id.txt_following);
-			btnViewProfile = (Button) view.findViewById(R.id.btn_view_profile);
+			btnAddStudymate = (Button) view.findViewById(R.id.btn_add_studymate);
 
 			txtUserName.setTypeface(myTypeFace.getRalewaySemiBold());
 			txtSchool.setTypeface(myTypeFace.getRalewayRegular());
 			txtLocation.setTypeface(myTypeFace.getRalewayRegular());
 			txtFollowing.setTypeface(myTypeFace.getRalewayRegular());
-			btnViewProfile.setTypeface(myTypeFace.getRalewayRegular());
+			btnAddStudymate.setTypeface(myTypeFace.getRalewayRegular());
 		}
 	}
 
 	@Override
 	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_your_studymate_request, parent, false));
+		return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_find_studymate_request, parent, false));
 	}
 
 	@Override
 	public void onBindViewHolder(ViewHolder holder, int position) {
-		holder.viewStatus.setBackgroundResource(position % 2 == 0 ? R.drawable.bg_online : R.drawable.bg_offline);
+		if (position % 2 == 0) {
+			holder.viewStatus.setBackgroundResource(R.drawable.bg_online);
+			holder.btnAddStudymate.setBackgroundResource(R.drawable.bg_button_dark_small);
+			holder.btnAddStudymate.setText(R.string.respond_to_request);
+		} else {
+			holder.viewStatus.setBackgroundResource(R.drawable.bg_offline);
+			holder.btnAddStudymate.setBackgroundResource(R.drawable.bg_button_green_small);
+			holder.btnAddStudymate.setText(R.string.add_studymate);
+		}
 		holder.txtUserName.setText("Daniel Golman");
 		holder.txtSchool.setText("Student from Lordes Convents");
 		holder.txtLocation.setText("Live in Ghana");
