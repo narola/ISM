4cce3546b4f18a537ac91fe7e70c39dfdf31fbff
Krunal Panchal <c161@C161s-Mac-Pro.local>
2015-12-01 18:24:21 +0530

    Tutorial discussion design

--- ISM/app/src/main/java/com/ism/fragment/TutorialDiscussionFragment.java
+++ ISM/app/src/main/java/com/ism/fragment/TutorialDiscussionFragment.java
@@ -1,13 +1,22 @@
 package com.ism.fragment;
 
 import android.app.Fragment;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.ism.R;
+import com.ism.adapter.DiscussionAdapter;
+import com.ism.model.TestDiscussion;
+import com.ism.object.MyTypeFace;
+
+import java.util.ArrayList;
 
 /**
  * Created by c161 on 12/10/15.
@@ -17,9 +26,12 @@ public class TutorialDiscussionFragment extends Fragment {
 	private static final String TAG = TutorialDiscussionFragment.class.getSimpleName();
 
 	private View view;
-	private TextView txtDay;
+	private TextView txtTopic, txtTopicValue;
+	private RecyclerView recyclerChat;
 
 	private TutorialDiscussionFragmentListener listenerTutorialDiscussion;
+	private ArrayList<TestDiscussion> arrListDiscussion;
+	private DiscussionAdapter adpDiscussion;
 
 	private static final String ARG_WEEK_DAY = "weekDay";
 	private int intWeekDay;
@@ -58,9 +70,50 @@ public class TutorialDiscussionFragment extends Fragment {
 	}
 
 	private void initGlobal() {
-		txtDay = (TextView) view.findViewById(R.id.txt_day);
+		txtTopic = (TextView) view.findViewById(R.id.txt_topic);
+		txtTopicValue = (TextView) view.findViewById(R.id.txt_topic_value);
+		recyclerChat = (RecyclerView) view.findViewById(R.id.recycler_chat);
+
+		MyTypeFace myTypeFace = new MyTypeFace(getActivity());
+		txtTopic.setTypeface(myTypeFace.getRalewayRegular());
+		txtTopicValue.setTypeface(myTypeFace.getRalewaySemiBold());
+
+		txtTopicValue.setText("Osmosis");
 
 		setDay(intWeekDay);
+
+		recyclerChat.setLayoutManager(new LinearLayoutManager(getActivity()));
+		RecyclerView.ItemDecoration decoration = new RecyclerView.ItemDecoration() {
+			@Override
+			public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+				super.getItemOffsets(outRect, view, parent, state);
+				if (parent.getChildLayoutPosition(view) == 0) {
+					outRect.top = 10;
+				}
+				outRect.left = 10;
+				outRect.right = 10;
+			}
+		};
+		recyclerChat.addItemDecoration(decoration);
+
+		arrListDiscussion = new ArrayList<>();
+		String time1 = "Aug 5, 2015  4:15pm";
+		String time2 = "Nov 25, 2015  7:10pm";
+		String time = time1;
+		for (int i = 0; i < 30; i++) {
+			TestDiscussion testDiscussion = new TestDiscussion();
+			if (i % 5 == 0) {
+				time = time.equals(time1) ? time2 : time1;
+				testDiscussion.setMessage("K");
+			} else {
+				testDiscussion.setMessage("Types of solutions, expression of concentration of solutions of solids in liquids, solubility of gases in liquids, solid solutions, colligative properties - relative lowering of vapour pressure, Raoult&#039;s law, elevation of boiling point, depression of freezing point, osmotic pressure, determination of molecular masses using colligative properties, abnormal molecular mass, Van&#039;t Hoff factor.");
+			}
+			testDiscussion.setTime(time);
+			testDiscussion.setUserName("Albert Crowley");
+			arrListDiscussion.add(testDiscussion);
+		}
+		adpDiscussion = new DiscussionAdapter(getActivity(), arrListDiscussion);
+		recyclerChat.setAdapter(adpDiscussion);
 	}
 
 	public void setTutorialDiscussionListener(TutorialDiscussionFragmentListener listenerTutorialDiscussion) {
@@ -71,25 +124,25 @@ public class TutorialDiscussionFragment extends Fragment {
 		intWeekDay = day;
 		switch (intWeekDay) {
 			case TutorialFragment.MON:
-				txtDay.setText("Monday");
+//				txtTopic.setText("Monday");
 				break;
 			case TutorialFragment.TUE:
-				txtDay.setText("Tuesday");
+//				txtTopic.setText("Tuesday");
 				break;
 			case TutorialFragment.WED:
-				txtDay.setText("Wednesday");
+//				txtTopic.setText("Wednesday");
 				break;
 			case TutorialFragment.THU:
-				txtDay.setText("Thursday");
+//				txtTopic.setText("Thursday");
 				break;
 			case TutorialFragment.FRI:
-				txtDay.setText("Friday");
+//				txtTopic.setText("Friday");
 				break;
 			case TutorialFragment.SAT:
-				txtDay.setText("Saturday");
+//				txtTopic.setText("Saturday");
 				break;
 			case TutorialFragment.SUN:
-				txtDay.setText("Sunday");
+//				txtTopic.setText("Sunday");
 				break;
 		}
 	}
---
+++ ISM/app/src/main/java/com/ism/model/TestDiscussion.java
@@ -0,0 +1,36 @@
+package com.ism.model;
+
+/**
+ * Created by c161 on 26/11/15.
+ */
+public class TestDiscussion {
+
+	private String time;
+	private String userName;
+	private String message;
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
