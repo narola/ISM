c062c9c1de4b64a1448ef135417f234ecb8d1cb2
Krunal <kd.narola@narolainfotech.com>
2015-12-08 09:28:10 +0530

    worked on socket

---
+++ ISM/ismsocket/src/main/java/com/socketdemo/c85/ismsocket/IOSocketHandler.java
@@ -0,0 +1,7 @@
+package com.socketdemo.c85.ismsocket;
+
+/**
+ * Created by c85 on 07/12/15.
+ */
+public class IOSocketHandler {
+}
---
+++ ISM/ismsocket/src/main/java/utils/SocketConstants.java
@@ -0,0 +1,7 @@
+package utils;
+
+/**
+ * Created by c85 on 07/12/15.
+ */
+public class SocketConstants {
+}
