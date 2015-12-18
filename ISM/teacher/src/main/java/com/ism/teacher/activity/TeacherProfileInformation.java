6bdcacf116d9113754470daba9fac79144c3432f
Ravi Yadav <c75@C75s-Mac-Pro.local>
2015-12-18 12:44:14 +0530

    ui changes in teacher registration form

--- ISM/teacher/src/main/java/com/ism/teacher/activity/TeacherProfileInformation.java
+++ ISM/teacher/src/main/java/com/ism/teacher/activity/TeacherProfileInformation.java
@@ -15,42 +15,50 @@ import android.widget.TextView;
 
 import com.ism.commonsource.view.ProgressGenerator;
 import com.ism.teacher.R;
-import com.ism.teacher.Utility.Utility;
+import com.ism.teacher.adapters.Adapters;
 import com.ism.teacher.adapters.CustomSpinnerAdapter;
 import com.ism.teacher.helper.InputValidator;
 import com.ism.teacher.helper.MyTypeFace;
+import com.ism.teacher.ws.helper.WebserviceWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by c75 on 15/10/15.
  */
-public class TeacherProfileInformation extends Activity implements View.OnClickListener {
+public class TeacherProfileInformation extends Activity implements WebserviceWrapper.WebserviceResponse {
 
     private static final String TAG = TeacherProfileInformation.class.getSimpleName();
-    //Views
-    private Spinner spGenderTeacher, spNameofSchool, spRegionSchool, spCityTeacher, spProgramCourse;
-    private Button btnSubmit, btnCancel;
 
-    private EditText etFullnameTeacher, etEmailAddressTeacher, etHomeAddressTeacher, etContactnoTeacher;
+    //Views
+    private Spinner spGender, spNameofSchool, spRegionSchool, spCity, spProgramCourse;
+    private Button btnSubmit, btnCancel;
+    private EditText etFullnameTeacher, etEmailAddressTeacher, etHomeAddressTeacher, etContactNumberTeacher, etSpecializaton, etEducation,
+            etDobTeacher;
     private EditText etUsername, etCurrentPassword, etNewPassword, etConfirmPassword;
     private LinearLayout llUploadProfilePic;
+    private TextView tvClickhere, tvUploadpic, tvPersonalInfo, tvWorkprofileInfo, tvYouare, tvIfits, tvManageCredential;
 
-    private TextView txtClickhere;
+    //arrays
     String strArrGender[] = {"Gender", "Female", "Male"};
     String strArrSchool[] = {"Name of School", "School1", "School2"};
     String strArrRegion[] = {"Region of the School", "Region1", "Region2"};
     String strArrCity[] = {"City", "City1", "City2"};
     String strArrProgram[] = {"Program/Course", "Program/Course1", "Program/Course"};
 
+    //Objects
 
     private CustomSpinnerAdapter spinnerAdapter;
     private boolean selected;
-    private LayoutInflater mInflator;
-
+    private LayoutInflater inflater;
     private InputValidator inputValidator;
-
-
     private MyTypeFace myTypeFace;
     ProgressGenerator progressGenerator;
+
+    //Array List
+    private List<String> arrListGender;
+    private List<String> arrListDefalt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,49 +73,91 @@ public class TeacherProfileInformation extends Activity implements View.OnClickL
 
     private void initView() {
 
-
         inputValidator = new InputValidator(TeacherProfileInformation.this);
-        txtClickhere = (TextView) findViewById(R.id.txt_clickhere);
+        tvClickhere = (TextView) findViewById(R.id.txt_clickhere);
+        tvUploadpic = (TextView) findViewById(R.id.txt_uploadpic);
+        tvPersonalInfo = (TextView) findViewById(R.id.tv_personal_info);
+
+        tvWorkprofileInfo = (TextView) findViewById(R.id.tv_workprofile_info);
+        tvYouare = (TextView) findViewById(R.id.txt_youare_);
+        tvIfits = (TextView) findViewById(R.id.txt_ifits_);
+        tvManageCredential = (TextView) findViewById(R.id.tv_manage_credential);
 
         llUploadProfilePic = (LinearLayout) findViewById(R.id.ll_upload_profile_pic);
 
-        etFullnameTeacher = (EditText) findViewById(R.id.et_fullname_teacher);
+        etDobTeacher = (EditText) findViewById(R.id.et_dob_teacher);
+        etFullnameTeacher = (EditText) findViewById(R.id.et_firstname_teacher);
         etEmailAddressTeacher = (EditText) findViewById(R.id.et_emailAdd_teacher);
         etHomeAddressTeacher = (EditText) findViewById(R.id.et_homeAdd_teacher);
-        etContactnoTeacher = (EditText) findViewById(R.id.et_contactno_teacher);
+        etContactNumberTeacher = (EditText) findViewById(R.id.et_contactno_teacher);
         etUsername = (EditText) findViewById(R.id.et_username);
         etCurrentPassword = (EditText) findViewById(R.id.et_currentpwd);
         etNewPassword = (EditText) findViewById(R.id.et_newpwd);
         etConfirmPassword = (EditText) findViewById(R.id.et_confirmpwd);
 
-        spGenderTeacher = (Spinner) findViewById(R.id.sp_gender_teacher);
+        etEducation = (EditText) findViewById(R.id.et_education);
+        etSpecializaton = (EditText) findViewById(R.id.et_specializaton);
+
+        spGender = (Spinner) findViewById(R.id.sp_gender_teacher);
         spNameofSchool = (Spinner) findViewById(R.id.sp_name_of_school);
         spRegionSchool = (Spinner) findViewById(R.id.sp_region_school);
-        spCityTeacher = (Spinner) findViewById(R.id.sp_city_teacher);
+        spCity = (Spinner) findViewById(R.id.sp_city_teacher);
         spProgramCourse = (Spinner) findViewById(R.id.sp_program_course);
 
         spinnerAdapter = new CustomSpinnerAdapter(TeacherProfileInformation.this, strArrGender);
         selected = false;
-        mInflator = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        setUpSpinner(spGenderTeacher, strArrGender);
-        setUpSpinner(spNameofSchool, strArrSchool);
-        setUpSpinner(spRegionSchool, strArrRegion);
-        setUpSpinner(spCityTeacher, strArrCity);
-        setUpSpinner(spProgramCourse, strArrProgram);
+        inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
+        arrListDefalt = new ArrayList<>();
+        arrListDefalt.add(getString(R.string.select));
+
+//        Adapters.setUpSpinner(TeacherProfileInformation.this, spCountry, arrListDefalt, myTypeFace.getRalewayRegular());
+//        Adapters.setUpSpinner(TeacherProfileInformation.this, spState, arrListDefalt, myTypeFace.getRalewayRegular());
+        Adapters.setUpSpinner(TeacherProfileInformation.this, spCity, arrListDefalt, myTypeFace.getRalewayRegular());
+
+        arrListGender = new ArrayList<>();
+        arrListGender.add(getString(R.string.strgender));
+        arrListGender.add(getString(R.string.male));
+        arrListGender.add(getString(R.string.female));
+
+        Adapters.setUpSpinner(TeacherProfileInformation.this, spGender, arrListGender, myTypeFace.getRalewayRegular());
+
+
+//        setUpSpinner(spNameofSchool, strArrSchool);
+//        setUpSpinner(spRegionSchool, strArrRegion);
+//        setUpSpinner(spProgramCourse, strArrProgram);
 
         btnSubmit = (Button) findViewById(R.id.btn_submit);
         btnCancel = (Button) findViewById(R.id.btn_cancel);
 
-        btnSubmit.setOnClickListener(this);
-        btnCancel.setOnClickListener(this);
-        txtClickhere.setOnClickListener(this);
-        llUploadProfilePic.setOnClickListener(this);
-
         myTypeFace = new MyTypeFace(this);
         progressGenerator = new ProgressGenerator();
 
+        applyFontsOnViews();
 
+    }
+
+    private void applyFontsOnViews() {
+
+        tvPersonalInfo.setTypeface(myTypeFace.getRalewayRegular());
+        tvUploadpic.setTypeface(myTypeFace.getRalewayRegular());
+        tvWorkprofileInfo.setTypeface(myTypeFace.getRalewayRegular());
+        tvYouare.setTypeface(myTypeFace.getRalewayRegular());
+        tvClickhere.setTypeface(myTypeFace.getRalewayRegular());
+        tvIfits.setTypeface(myTypeFace.getRalewayRegular());
+        tvManageCredential.setTypeface(myTypeFace.getRalewayRegular());
+
+        etDobTeacher.setTypeface(myTypeFace.getRalewayRegular());
+        etFullnameTeacher.setTypeface(myTypeFace.getRalewayRegular());
+        etEmailAddressTeacher.setTypeface(myTypeFace.getRalewayRegular());
+        etHomeAddressTeacher.setTypeface(myTypeFace.getRalewayRegular());
+        etContactNumberTeacher.setTypeface(myTypeFace.getRalewayRegular());
+        etSpecializaton.setTypeface(myTypeFace.getRalewayRegular());
+        etEducation.setTypeface(myTypeFace.getRalewayRegular());
+        etUsername.setTypeface(myTypeFace.getRalewayRegular());
+        etCurrentPassword.setTypeface(myTypeFace.getRalewayRegular());
+        etConfirmPassword.setTypeface(myTypeFace.getRalewayRegular());
+        etNewPassword.setTypeface(myTypeFace.getRalewayRegular());
     }
 
 
@@ -136,7 +186,7 @@ public class TeacherProfileInformation extends Activity implements View.OnClickL
                                         ViewGroup parent) {
                 View v = super.getDropDownView(position, convertView,
                         parent);
-                v.setBackgroundResource(R.color.color_white);
+                v.setBackgroundResource(R.color.white);
 
                 ((TextView) v).setTextColor(getResources().getColorStateList(
                         R.color.color_dark_gray));
@@ -144,7 +194,7 @@ public class TeacherProfileInformation extends Activity implements View.OnClickL
                 ((TextView) v).setCompoundDrawables(null, null, null, null);
                 if (position == 0) {
                     ((TextView) v).setTextColor(getResources().getColorStateList(
-                            R.color.color_white));
+                            R.color.white));
                     ((TextView) v).setBackgroundColor(getResources().getColor(R.color.color_blue));
                     ((TextView) v).setEnabled(false);
 
@@ -158,31 +208,39 @@ public class TeacherProfileInformation extends Activity implements View.OnClickL
         spinner.setAdapter(adapter);
     }
 
-    @Override
-    public void onClick(View view) {
-
-        if (view == btnSubmit) {
-//            if (isTextFieldsInputsValid()) {
-//                Global.launchIntent(TeacherProfileInformation.this, TeacherHostActivity.class);
-//            }
-            Utility.launchIntent(TeacherProfileInformation.this, TeacherHostActivity.class);
-
-        } else if (view == llUploadProfilePic) {
-
-        } else if (view == btnCancel) {
-
-        } else if (view == txtClickhere) {
-        }
-    }
+//    @Override
+//    public void onClick(View view) {
+//
+//        if (view == btnSubmit) {
+////            if (isTextFieldsInputsValid()) {
+////                Global.launchIntent(TeacherProfileInformation.this, TeacherHostActivity.class);
+////            }
+//            Utility.launchIntent(TeacherProfileInformation.this, TeacherHostActivity.class);
+//
+//        } else if (view == llUploadProfilePic) {
+//
+//
+//        } else if (view == btnCancel) {
+//
+//        } else if (view == tvClickhere) {
+//        }
+//    }
 
     private boolean isTextFieldsInputsValid() {
         return inputValidator.validateStringPresence(etFullnameTeacher)
+                & inputValidator.validateStringPresence(etEducation)
+                & inputValidator.validateStringPresence(etSpecializaton)
                 & (inputValidator.validateStringPresence(etEmailAddressTeacher) && inputValidator.validateEmail(etEmailAddressTeacher))
-                & (inputValidator.validateStringPresence(etContactnoTeacher) && inputValidator.validatePhoneNumberLength(etContactnoTeacher))
+                & (inputValidator.validateStringPresence(etContactNumberTeacher) && inputValidator.validatePhoneNumberLength(etContactNumberTeacher))
                 & inputValidator.validateStringPresence(etHomeAddressTeacher)
                 & inputValidator.validateStringPresence(etUsername)
                 & (inputValidator.validateStringPresence(etCurrentPassword) && inputValidator.validatePasswordLength(etHomeAddressTeacher))
                 & (inputValidator.validateStringPresence(etNewPassword) && inputValidator.validatePasswordLength(etNewPassword))
                 & (inputValidator.validateStringPresence(etConfirmPassword) && inputValidator.validatePasswordLength(etConfirmPassword) && inputValidator.validateConfirmPasswordMatch(etNewPassword, etConfirmPassword));
+    }
+
+    @Override
+    public void onResponse(int API_METHOD, Object object, Exception error) {
+
     }
 }
--- ISM/teacher/src/main/java/com/ism/teacher/adapters/Adapters.java
+++ ISM/teacher/src/main/java/com/ism/teacher/adapters/Adapters.java
@@ -1,6 +1,7 @@
 package com.ism.teacher.adapters;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -29,6 +30,52 @@ public class Adapters {
     static Integer layout;
 
 
+    public static void setUpSpinner(final Context context, Spinner spinner, final List<String> strArr, final Typeface typeface) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, R.layout.simple_spinner, strArr) {
+
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                return getTextView(context, position, typeface, textView);
+            }
+
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                TextView textView = (TextView) super.getDropDownView(position, convertView, parent);
+                setTextViewDropdown(context, position, typeface, textView);
+                return textView;
+            }
+        };
+        adapter.setDropDownViewResource(R.layout.row_spinner);
+        spinner.setAdapter(adapter);
+    }
+
+    private static void setTextViewDropdown(Context context, int position, Typeface typeface, TextView textView) {
+        if (typeface != null) {
+            textView.setTypeface(typeface);
+        }
+        textView.setCompoundDrawables(null, null, null, null);
+        if (position == 0) {
+            textView.setTextColor(context.getResources().getColorStateList(R.color.white));
+            textView.setBackgroundColor(context.getResources().getColor(R.color.color_green));
+            textView.setEnabled(false);
+            textView.setOnClickListener(null);
+        } else {
+            textView.setTextColor(context.getResources().getColorStateList(R.color.color_dark_gray));
+            textView.setBackgroundResource(R.color.white);
+        }
+    }
+
+    private static View getTextView(Context context, int position, Typeface typeface, TextView textView) {
+        if (typeface != null) {
+            textView.setTypeface(typeface);
+        }
+        if (position == 0) {
+            textView.setTextColor(context.getResources().getColor(R.color.color_text_hint));
+            return textView;
+        } else {
+            textView.setTextColor(context.getResources().getColor(R.color.color_black));
+            return textView;
+        }
+    }
     //Question List
     public static void setUpSpinner(final Context context, Spinner spinner, List<String> strArr, int ADAPTER_TYPE) {
         final MyTypeFace myTypeFace = new MyTypeFace(context);
@@ -64,13 +111,13 @@ public class Adapters {
                 textView.setCompoundDrawables(null, null, null, null);
 
                 if (position == 0) {
-                    textView.setTextColor(context.getResources().getColorStateList(R.color.color_white));
+                    textView.setTextColor(context.getResources().getColorStateList(R.color.white));
                     textView.setBackgroundColor(context.getResources().getColor(R.color.color_blue));
                     textView.setEnabled(false);
                     textView.setOnClickListener(null);
                 } else {
                     textView.setTextColor(context.getResources().getColorStateList(R.color.color_dark_gray));
-                    textView.setBackgroundResource(R.color.color_white);
+                    textView.setBackgroundResource(R.color.white);
                 }
 
                 return textView;
@@ -103,13 +150,13 @@ public class Adapters {
                 textView.setCompoundDrawables(null, null, null, null);
 
                 if (position == 0) {
-                    textView.setTextColor(context.getResources().getColorStateList(R.color.color_white));
+                    textView.setTextColor(context.getResources().getColorStateList(R.color.white));
                     textView.setBackgroundColor(context.getResources().getColor(R.color.color_blue));
                     textView.setEnabled(false);
                     textView.setOnClickListener(null);
                 } else {
                     textView.setTextColor(context.getResources().getColorStateList(R.color.color_dark_gray));
-                    textView.setBackgroundResource(R.color.color_white);
+                    textView.setBackgroundResource(R.color.white);
                 }
 
                 return textView;
