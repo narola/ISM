996e3fe71a1125e32f427c935a02c00aa72b073e
Pankaj Vasava <pv.narola@narolainfotech.com>
2016-04-04 17:18:05 +0530

    Fixed issue for feed post on load more.(before it skips 4 post on load more)

--- phase1/websocket/class.PHPWebSocket.php
+++ phase1/websocket/class.PHPWebSocket.php
@@ -1244,7 +1244,13 @@ class PHPWebSocket {
 	    if (!empty($data['start']) && is_numeric($data['start'])) {
 		$limit = 4;
 		$ID_in = implode(',', $this->class_mate_list($user_id));
-		$data['start'] += $limit;
+			if($data['start'] == $data['start'])
+			{
+				$data['start'] = $limit;
+			}else
+			{
+				$data['start'] += $limit;
+			}
 		$query = "SELECT `f`.`id` as `post_id`, `f`.`feed_by`, `f`.`feed_text` as `message`, `f`.`created_date` as `posted_on`,"
 			. " `u`.`full_name` , `l`.`is_delete` as my_like ,"
 			// . " `u`.`full_name`,`u`.`id` , `l`.`is_delete` as my_like ,"
